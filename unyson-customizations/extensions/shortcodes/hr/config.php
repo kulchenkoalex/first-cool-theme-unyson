d9fe0320235bdda4afba9cb17f032038f4bdfbf2
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-30 15:45:31 +0300

    Создание нового шорткода: Cookbook: config.php

---
+++ unyson-customizations/extensions/shortcodes/hr/config.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('FW')) {
+    die('Forbidden');
+}
+
+$cfg = array(
+    'page_builder' => array(
+        'title'       => __('Horizontal Ruler', '{domain}'),
+        'description' => __('Creates a \'hr\' html tag', '{domain}'),
+        'tab'         => __('Demo Elements', '{domain}'),
+    )
+);
