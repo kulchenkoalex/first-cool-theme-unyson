714266304c8bd604e26e9a5395aa5dcb9070edd7
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-30 14:40:18 +0300

    Создание нового шорткода: config.php

---
+++ unyson-customizations/extensions/shortcodes/my-shortcode/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$cfg = array(
+    'page_builder' => array(
+        'title'         => __('My ext Shortcode', '{domain}'),
+        'description'   => __('My ext shortcode description', '{domain}'),
+        'tab'           => __('My ext Elements', '{domain}'),
+        'popup_size'    => 'small', 
+    )
+);
