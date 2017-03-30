f6ac3bea4e6ea42d802cddfd782536415278fbd8
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-30 15:53:45 +0300

    Создание нового шорткода: Cookbook: button shortcode config.php

---
+++ unyson-customizations/extensions/shortcodes/my-button/config.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('FW')) {
+    die('Forbidden');
+}
+
+$cfg = array(
+    'page_builder' => array(
+        'title'         => __('Button', '{domain}'),
+        'description'   => __('Creates a button with choosable label, size and style', '{domain}'),
+        'tab'           => __('Demo Elements', '{domain}'),
+    )
+);
