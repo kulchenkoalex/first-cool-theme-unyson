02f8b5d5fc6da3176c2c7d235ee22f07e00b9069
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-30 16:09:14 +0300

    Создание нового шорткода: Cookbook: button shortcode options.php

---
+++ unyson-customizations/extensions/shortcodes/my-button/options.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!defined('FW')) {
+    die('Forbidden');
+}
+
+$options = array(
+    'label' => array(
+        'label'   => __('Label', '{domain}'),
+        'desc'    => __('The button label', '{domain}'),
+        'type'    => 'text',
+        'value'   => __('Click me!', '{domain}')
+    ),
+    'size' => array(
+        'label'   => __('Size', '{domain}'),
+        'desc'    => __('The button size', '{domain}'),
+        'type'    => 'select',
+        'choices' => array(
+            'big'    => __('Big', '{domain}'),
+            'medium' => __('Medium', '{domain}'),
+            'small'  => __('Small', '{domain}')
+        ),
+        'value'   => 'medium'
+    ),
+    'style' => array(
+        'label'   => __('Style', '{domain}'),
+        'desc'    => __('The button style', '{domain}'),
+        'type'    => 'select',
+        'choices' => array(
+            'primary'   => __('Primary', '{domain}'),
+            'secondary' => __('Secondary', '{domain}')
+        )
+    )
+);
