50e50a483f40442a33841fdeeea3c8bbac044b24
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-30 15:03:08 +0300

    Создание нового шорткода: static файл static.php

---
+++ unyson-customizations/extensions/shortcodes/my-shortcode/static.php
@@ -0,0 +1,14 @@
+<?php 
+if (!defined('FW')) 
+	die('Forbidden');
+
+// find the uri to the shortcode folder
+$uri = fw_get_template_customizations_directory_uri('/extensions/shortcodes/my-shortcode');
+wp_enqueue_style(
+    'fw-shortcode-demo-shortcode',
+    $uri . '/static/css/styles.css'
+);
+wp_enqueue_script(
+    'fw-shortcode-demo-shortcode',
+    $uri . '/static/js/scripts.js'
+);
