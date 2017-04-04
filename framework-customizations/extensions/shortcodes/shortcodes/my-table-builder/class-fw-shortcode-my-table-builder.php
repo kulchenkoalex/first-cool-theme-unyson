23a67bed50ba7b061722ed9160920d6f2b80fbee
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-04-04 09:52:15 +0300

    Изменение путей

---
+++ framework-customizations/extensions/shortcodes/shortcodes/my-table-builder/class-fw-shortcode-my-table-builder.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined('FW')) {
+    die('Forbidden');
+}
+
+class FW_Shortcode_My_Table_Builder extends FW_Shortcode
+{
+    /**
+     * @internal
+     */
+    public function _init()
+    {
+        if (is_admin()) {
+            $this->load_option_type();
+        }
+    }
+    private function load_option_type()
+    {
+       // require $this->locate_path('/includes/option-types/my-table-builder/class-fw-option-type-my-table-builder.php');
+    }
+}
--- unyson-customizations/extensions/myextension/class-fw-extension-myextension.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('FW')) die('Forbidden');
-
-class FW_Extension_{Extension_Name} extends FW_Extension
-{
-    public function _init() {
-    }
-}
